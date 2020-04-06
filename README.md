e5b384455cf032fc10f6370af6ce7e23798e1b33
Dev <dmb4086@rit.edu>
2020-04-06 00:31:31 -0400

    Main Method code updated

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ to study the behavior of search algorithms across various sets of inputs using a
 Monte Carlo analysis involves subjecting a system to random input perturbations in order to see how it responds 
 across a wide variety of inputs. For this Repository my purpose is to focus on studying how the average number of operations of three 
 search algorithms varies across a collection of randomly generated data sets.Specifically, I will be analyzing 
-linear search, binary search, and interpolation search in terms of the average number of operation seach algorithm 
-performson average over a series of data sets.
+linear search, binary search, and interpolation search in terms of the average number of operation search algorithm 
+performs on average over a series of data sets.
 
 
--- src/ClassicSearchesA3.java
+++ src/ClassicSearchesA3.java
@@ -1,4 +1,9 @@
+import com.sun.security.jgss.GSSUtil;
+
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class ClassicSearchesA3 {
     static int ops = 0;
@@ -43,7 +48,7 @@ public class ClassicSearchesA3 {
         int high = arr.length - 1;
         ops = 0;
 
-        while(low <= high) {
+        while(low <= high && key >= arr[low] && key <= arr[high]) {
             ops++;
             int index = low + (((key - arr[low]) * (high - low)) /
                     (arr[high] - arr[low]));
@@ -78,16 +83,163 @@ public class ClassicSearchesA3 {
         System.out.println();
     }
 
+    /*
+    This function returns an Array of size 1000 with random values from 1 - 10,000
+
+     */
+    static int[] ArrGenerator(){
+        Random rd = new Random();
+        int low = 1;
+        int high = 10000;
+
+        int[] arr = new int[1000];
+
+        for (int i = 0 ; i<arr.length; i++){
+            int result = rd.nextInt(high-low) + low;
+            arr[i] = result;
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+
     public static void main(String[] args) {
         int index;
         int key;
-        int[] nums = {15, 98, 7, 22, 9, 61, 57};
+//        int[] nums = {15, 98, 7, 22, 9, 61, 57};
+        int nums[][] = new int[100][];
 
-        Arrays.sort(nums);
-        printArray(nums);
-        key = 57;
-        searchResult("Linear", key, linearSearchOrdered(nums, key));
-        searchResult("Binary", key, binarySearch(nums, key));
-        searchResult("Interpolation", key, interpolationSearch(nums, key));
+
+
+        for (int i = 0; i<100;i++){
+            nums[i] = ArrGenerator();
+
+        }
+
+
+
+
+//        to print the 2D array
+//        System.out.println(Arrays.deepToString(nums));
+
+        ArrayList<Double> LinearAvg = new ArrayList<Double>();
+        ArrayList<Double> InterAvg = new ArrayList<Double>();
+        ArrayList<Double> BinaryAvg = new ArrayList<Double>();
+
+        for(int i = 0 ; i<100; i++){
+            Double TotalBinaryOps = 0.0;
+            Double TotalInterOps = 0.0;
+            Double TotalLinearOps = 0.0;
+
+            for (int j =0 ; j<1000; j++){
+                key = nums[i][j];
+//                searchResult("Linear", key, linearSearchOrdered(nums[i], key));
+//                searchResult("Binary", key, binarySearch(nums[i], key));
+//                searchResult("Interpolation", key, interpolationSearch(nums[i], key));
+
+                linearSearchOrdered(nums[i],key);
+                TotalLinearOps += ops;
+                binarySearch(nums[i],key);
+                TotalBinaryOps += ops;
+                interpolationSearch(nums[i],key);
+                TotalInterOps += ops;
+            }
+            LinearAvg.add(TotalLinearOps/1000);
+            BinaryAvg.add(TotalBinaryOps/1000);
+            InterAvg.add (TotalInterOps/1000);
+
+        }
+        Double LinAverage = 0.0;
+        for (Double item: LinearAvg) {
+            LinAverage+=item;
+        }
+        Double BinAverage = 0.0;
+        for (Double item: BinaryAvg) {
+            BinAverage+=item;
+        }
+        Double ItnAverage = 0.0;
+        for (Double item: InterAvg) {
+            ItnAverage+=item;
+        }
+        LinAverage/= 100;
+        ItnAverage/= 100;
+        BinAverage/= 100;
+
+        System.out.println("Linear Average = "+LinAverage);
+        System.out.println("Interpolation Average = "+ItnAverage);
+        System.out.println("Binary Average = "+ BinAverage);
+
+        int num2 [] = new int[1000];
+        for (int count =0 ; count < 1000; count ++){
+            boolean flag = false;
+            int Number = (int)(Math.random() * (1000 +1) );
+            for (int j = 0; j < 100; j++) {
+                if(interpolationSearch(nums[j], Number) != -1){
+                    flag = true;
+                }
+            }
+            if(flag){
+                num2[count] = Number;
+                count++;
+            }
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------------------
+
+        ArrayList<Double> unsucessfullLinearAvg = new ArrayList<Double>();
+        ArrayList<Double> unsucessfullInterAvg = new ArrayList<Double>();
+        ArrayList<Double> unsucessfullBinaryAvg = new ArrayList<Double>();
+
+        for(int i = 0 ; i<100; i++){
+            Double unsucessfullTotalBinaryOps = 0.0;
+            Double unsucessfullTotalInterOps = 0.0;
+            Double unsucessfullTotalLinearOps = 0.0;
+
+            for (int j =0 ; j<1000; j++){
+                key = num2[i];
+//                searchResult("Linear", key, linearSearchOrdered(nums[i], key));
+//                searchResult("Binary", key, binarySearch(nums[i], key));
+//                searchResult("Interpolation", key, interpolationSearch(nums[i], key));
+
+                linearSearchOrdered(nums[i],key);
+                unsucessfullTotalLinearOps += ops;
+                binarySearch(nums[i],key);
+                unsucessfullTotalBinaryOps += ops;
+                interpolationSearch(nums[i],key);
+                unsucessfullTotalInterOps += ops;
+            }
+            unsucessfullLinearAvg.add(unsucessfullTotalLinearOps/1000);
+            unsucessfullBinaryAvg.add(unsucessfullTotalBinaryOps/1000);
+            unsucessfullInterAvg.add (unsucessfullTotalInterOps/1000);
+
+        }
+        Double unsucessfullTotalLinearOps = 0.0;
+        for (Double item: unsucessfullLinearAvg) {
+            unsucessfullTotalLinearOps+=item;
+        }
+        Double unsucessfullTotalBinaryOps = 0.0;
+        for (Double item: unsucessfullBinaryAvg) {
+            unsucessfullTotalBinaryOps+=item;
+        }
+        Double unsucessfullTotalInterOps = 0.0;
+        for (Double item: unsucessfullInterAvg) {
+            unsucessfullTotalInterOps+=item;
+        }
+        unsucessfullTotalLinearOps/= 100;
+        unsucessfullTotalInterOps/= 100;
+        unsucessfullTotalBinaryOps/= 100;
+
+        System.out.println("Linear Average = "+unsucessfullTotalLinearOps);
+        System.out.println("Interpolation Average = "+unsucessfullTotalInterOps);
+        System.out.println("Binary Average = "+ unsucessfullTotalBinaryOps);
+
+
+
+
+
+
+
     }
+
+
+
 }
